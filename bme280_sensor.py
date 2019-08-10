1fe7d9b6a08b02954f050cd91e76f713e3abae2b
Toon Spin <toon.spin@gmail.com>
2019-08-10 21:44:52 +0100

    Make Python script suitable for Python 3

--- bme280_sensor.py
+++ bme280_sensor.py
@@ -13,5 +13,5 @@ while True:
     humidity  = bme280_data.humidity
     pressure  = bme280_data.pressure
     ambient_temperature = bme280_data.temperature
-    print(humidity, pressure, ambient_temperature)
+    print((humidity, pressure, ambient_temperature))
     sleep(1)
