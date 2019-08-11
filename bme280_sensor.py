adcb497e66a87f809111be19cda4a29be37652ee
Toon Spin <toon.spin@gmail.com>
2019-08-11 17:21:10 +0100

    Add better error handling to Python script

--- bme280_sensor.py
+++ bme280_sensor.py
@@ -5,6 +5,7 @@ import sqlite3
 from time import sleep
 from pathlib import Path
 from os import environ
+from sys import exit
 
 
 database_file = "./sensor-data.db"
@@ -21,26 +22,36 @@ if 'SENSOR_DATA_PORT' in environ:
     port = environ['SENSOR_DATA_PORT']
 
 
+try:
+    bus = smbus2.SMBus(port)
+    bme280.load_calibration_params(bus,address)
+except OSError:
+    print("Error communicating with sensor!")
+    exit(1)
+
+
 db_file = Path(database_file)
 if not db_file.is_file():
-    db = sqlite3.connect(database_file)
-    cursor = db.cursor()
-    cursor.execute('''
-        CREATE TABLE sensor_data (
-            id INTEGER PRIMARY KEY,
-            "timestamp" DATETIME,
-            humidity FLOAT(32),
-            pressure FLOAT(32),
-            temperature FLOAT(32)
-        );
-    ''')
-    cursor.execute('CREATE INDEX timestamp_ind ON sensor_data(timestamp);')
-    db.commit()
-    db.close()
+    try:
+        db = sqlite3.connect(database_file)
+        cursor = db.cursor()
+        cursor.execute('''
+            CREATE TABLE sensor_data (
+                id INTEGER PRIMARY KEY,
+                "timestamp" DATETIME,
+                humidity FLOAT(32),
+                pressure FLOAT(32),
+                temperature FLOAT(32)
+            );
+        ''')
+        cursor.execute('CREATE INDEX timestamp_ind ON sensor_data(timestamp);')
+        db.commit()
+        db.close()
+    except sqlite3.Error:
+        print("Error creating database!")
+        exit(1)
 
 
-bus = smbus2.SMBus(port)
-bme280.load_calibration_params(bus,address)
 
 db = sqlite3.connect(database_file)
 cursor = db.cursor()
@@ -48,6 +59,9 @@ row_insert_query = 'INSERT INTO sensor_data ("timestamp", humidity, pressure, te
 
 while True:
     bme280_data = bme280.sample(bus,address)
-    cursor.execute(row_insert_query, (bme280_data.humidity, bme280_data.pressure, bme280_data.temperature))
-    db.commit()
+    try:
+        cursor.execute(row_insert_query, (bme280_data.humidity, bme280_data.pressure, bme280_data.temperature))
+        db.commit()
+    except sqlite3.Error:
+        print("Error writing measurement to database!")
     sleep(1)
